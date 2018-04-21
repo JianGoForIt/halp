c855be6f0abb30c7a7e01da731bd4afa17b5aff4
zjian <zjian@stanford.edu>
2018-04-21 00:22:27 -0700

    setup for lp kernel experiment with lp sgd

--- optim/__init__.py
+++ optim/__init__.py
@@ -1,2 +1,3 @@
 from svrg import SVRG 
 from halp import HALP
+from lpsgd import LPSGD
--- optim/halp.py
+++ optim/halp.py
@@ -4,7 +4,7 @@ from torch.autograd import Variable
 import copy, logging
 import math
 
-import quantize
+from .. import quantize
 
 class HALP(torch.optim.SGD):
     """Implements high-accuracy low-precision algorithm.
