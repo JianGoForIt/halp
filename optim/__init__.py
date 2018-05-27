ca0251b835e88db819d81315cf394074cbd4f329
Jian Zhang <zhangjianthu@gmail.com>
2018-05-27 07:52:31 +0000

    add bit centering lmsgd algorithm

--- optim/__init__.py
+++ optim/__init__.py
@@ -2,6 +2,7 @@ from .svrg import SVRG
 from .halp import HALP
 from .lpsgd import LPSGD
 from .bit_center_lpsgd import BitCenterLPSGD
+from .bit_center_lmsgd import BitCenterLMSGD
 from .lm_halp import LMHALP
 
 #from svrg import SVRG 
